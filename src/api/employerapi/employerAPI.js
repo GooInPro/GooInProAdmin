feaec30ff994b57b3b5a579a58ff13c7ec54aef8
jhyeong <hyeongcode@gmail.com>
2025-01-13 18:10:04 +0900

    clore: employer approve column remove

--- src/api/employerapi/employerAPI.js
+++ src/api/employerapi/employerAPI.js
@@ -2,22 +2,12 @@ import axios from "axios";
 
 const host = `${import.meta.env.VITE_API_HOST}/employer`;
 
-//승인 된 고용인 리스트 호출 api
-export const getApprovedEmployerList = async (page) => {
+//고용인 리스트 호출 api
+export const getEmployerList = async (page) => {
 
     const pageValue = (Number)(page || 1)
 
-    const res = await axios.get(`${host}/approvedList?page=${pageValue}`)
-
-    return res.data;
-}
-
-//승인되지 않은 고용인 리스트 호출 api
-export const getNotApprovedEmployerList = async (page) => {
-
-    const pageValue = (Number)(page || 1)
-
-    const res = await axios.get(`${host}/notApprovedList?page=${pageValue}`)
+    const res = await axios.get(`${host}/list?page=${pageValue}`)
 
     return res.data;
 }
