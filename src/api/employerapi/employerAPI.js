17719501ad48a8c4d5853b138f4b3fcc1b9f92c5
jhyeong <hyeongcode@gmail.com>
2025-01-15 13:48:55 +0900

    feat: Employer delete api add

--- src/api/employerapi/employerAPI.js
+++ src/api/employerapi/employerAPI.js
@@ -5,9 +5,20 @@ const host = `${import.meta.env.VITE_API_HOST}/employer`;
 //고용인 리스트 호출 api
 export const getEmployerList = async (page) => {
 
-    const pageValue = (Number)(page || 1)
+    const pageValue = (Number)(page || 1);
 
-    const res = await axios.get(`${host}/list?page=${pageValue}`)
+    const res = await axios.get(`${host}/list?page=${pageValue}`);
+
+    console.log('getEmployerList' + res.data);
+
+    return res.data;
+}
+
+export const deleteEmployer = async (eno) => {
+
+    const res = await axios.put(`${host}/delete/${eno}`);
+
+    console.log('deleteEmployer' + res.data);
 
     return res.data;
 }
