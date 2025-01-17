d6716afe2b2b1504040848ef274600742206e922
jhyeong <hyeongcode@gmail.com>
2025-01-17 15:00:47 +0900

    feat: Employer Read Page + JobPosting List Component

--- src/pages/jobpostingspages/JobPostingsListPage.vue
+++ src/pages/jobpostingspages/JobPostingsListPage.vue
@@ -4,7 +4,7 @@ import JobPostingsListComponent from "../../components/jobpostingscomponents/Job
 </script>
 
 <template>
-  <JobPostingsListComponent/>
+  <JobPostingsListComponent eno="0"/>
 </template>
 
 <style scoped>
